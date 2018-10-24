9816a862c883757e7b5a358d0a538cc8f8425e07
Jovo <sunjkajovo@gmail.com>
2018-10-24 16:39:14 +0200

    Inicijalni projekat okacen

---
+++ src/main/java/com/mjvs/jgsp/JgspApplication.java
@@ -0,0 +1,12 @@
+package com.mjvs.jgsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgspApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mjvs/jgsp/JgspApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mjvs.jgsp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JgspApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
